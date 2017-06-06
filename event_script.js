9bc8c3c4f91bc48866a3bf83ce76cff3b10b489d
Carlos G. Gavidia <cgavidiac@gmail.com>
2017-06-06 18:53:29 +0100

    Mining the bug reporters

--- event_script.js
+++ event_script.js
@@ -2,27 +2,45 @@
  * Created by Carlos G. Gavidia on 06/06/2017.
  */
 
-var xhr = new XMLHttpRequest();
 
-function onReadyStateChange() {
-    if (xhr.readyState == 4) {
-        console.log(xhr.responseText);
-    }
-}
 
-//TODO(cgavidia): Temporary values for testing.
+//TODO: Temporary values for testing.
 var project = "OPENJPA";
 
 var jql = "project=" + project + "+order+by+created+desc";
 var status = "open";
 var maxResults = "5";
-//TODO(cgavidia): Include the parameter "fields" in the request
+//TODO: Include the parameter "fields" in the request
+//TODO: This requires that the user is already logged in the target JIRA system.
 
 var host = "https://issues.apache.org/jira";
 var searchService = host + "/rest/api/2/search?";
 var openIssuesQueryString = "jql=" + jql + "&status=" + status + "&maxResults=" + maxResults;
 
 console.log("Getting open issues using: " + searchService + openIssuesQueryString);
-xhr.onreadystatechange = onReadyStateChange;
-xhr.open("GET", searchService + openIssuesQueryString);
-xhr.send();
+
+var xmlHttpRequest = new XMLHttpRequest();
+var url = searchService + openIssuesQueryString;
+xmlHttpRequest.onreadystatechange = function () {
+    "use strict";
+    if (xmlHttpRequest.readyState === 4 && xmlHttpRequest.status === 200) {
+        var parsedResponse = JSON.parse(xmlHttpRequest.responseText);
+        console.log("parsedResponse", parsedResponse);
+
+        var reporters = {};
+        var reporter;
+
+        parsedResponse.issues.forEach(function (issue) {
+            console.log("issue", issue);
+
+            reporter = issue.fields.reporter.name;
+            reporters[reporter] = true;
+        });
+
+        console.log("reporters", reporters);
+    }
+
+};
+
+xmlHttpRequest.open("GET", url, true);
+xmlHttpRequest.send();
