76bc296b70aa29cb979d4c22849cf681813b4f81
Carlos G. Gavidia <cgavidiac@gmail.com>
2017-06-06 21:51:37 +0100

    Preparing data to show

--- event_script.js
+++ event_script.js
@@ -12,7 +12,7 @@ var inflationPenalty = 0.1;
 var projectJql = "project=" + project + "+order+by+created+desc";
 var openStatus = "open";
 var resolvedStatus = "resolved";
-var maxResults = "100";
+var maxResults = "20";
 //TODO: Include the parameter "fields" in the request
 //TODO: This requires that the user is already logged in the target JIRA system.
 //TODO: We need to figure out how necessary is this maxResults parameter
@@ -21,12 +21,13 @@ var host = "https://issues.apache.org/jira";
 var searchService = host + "/rest/api/2/search?";
 var openIssuesQueryString = "jql=" + projectJql + "&status=" + openStatus + "&maxResults=" + maxResults;
 
+var unassignedIssues = null;
 var reputationScore = {};
 
-function startDefaultReputationMap(parsedResponse) {
+function startDefaultReputationMap() {
     "use strict";
     var reporter;
-    parsedResponse.issues.forEach(function (issue) {
+    unassignedIssues.forEach(function (issue) {
         reporter = issue.fields.reporter.name;
         reputationScore[reporter] = maximumReputation;
     });
@@ -35,6 +36,33 @@ function startDefaultReputationMap(parsedResponse) {
 function reputationScoresReady() {
     "use strict";
     console.log("READY: reputationScore", reputationScore);
+
+    var unassigedIssueList = [];
+    unassignedIssues.forEach(function (issue) {
+        unassigedIssueList.push({
+            key: issue.key,
+            summary: issue.fields.summary,
+            reporter: issue.fields.reporter.name,
+            reporterScore: reputationScore[issue.fields.reporter.name],
+            priorityId: issue.fields.priority.id, //TODO: Not sure if this ID is sorted in the hierarchy
+            priorityName: issue.fields.priority.name
+        });
+    });
+
+    unassigedIssueList.sort(function (issue, otherIssue) {
+        if (issue.reporterScore > otherIssue.reporterScore) {
+            return -1;
+        }
+
+        if (issue.reporterScore < otherIssue.reporterScore) {
+            return 1;
+        }
+
+        return 0;
+    });
+
+    console.log("unassigedIssueList", unassigedIssueList);
+
 }
 
 function updateReportersReputation(reporterName, potentialInflatedIssues) {
@@ -77,10 +105,10 @@ var openIssuesUrl = searchService + openIssuesQueryString;
 openIssuesXhr.onreadystatechange = function () {
     "use strict";
     if (openIssuesXhr.readyState === 4 && openIssuesXhr.status === 200) {
-        var parsedResponse = JSON.parse(openIssuesXhr.responseText);
-        console.log("parsedResponse", parsedResponse);
+        unassignedIssues = JSON.parse(openIssuesXhr.responseText).issues;
+        console.log("unassignedIssues", unassignedIssues);
 
-        startDefaultReputationMap(parsedResponse);
+        startDefaultReputationMap();
 
         console.log("Default reputationScore", reputationScore);
         Object.keys(reputationScore).forEach(getReportersReputation);
