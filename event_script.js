dc571c7b883206c52ee506d1bbd448fd624c9b87
Carlos G. Gavidia <cgavidiac@gmail.com>
2017-06-06 21:05:18 +0100

    Including potential inflations

--- event_script.js
+++ event_script.js
@@ -7,40 +7,69 @@
 //TODO: Temporary values for testing.
 var project = "OPENJPA";
 
-var jql = "project=" + project + "+order+by+created+desc";
-var status = "open";
-var maxResults = "5";
+var projectJql = "project=" + project + "+order+by+created+desc";
+var openStatus = "open";
+var resolvedStatus = "resolved";
+var maxResults = "100";
 //TODO: Include the parameter "fields" in the request
 //TODO: This requires that the user is already logged in the target JIRA system.
+//TODO: We need to figure out how necessary is this maxResults parameter
 
 var host = "https://issues.apache.org/jira";
 var searchService = host + "/rest/api/2/search?";
-var openIssuesQueryString = "jql=" + jql + "&status=" + status + "&maxResults=" + maxResults;
+var openIssuesQueryString = "jql=" + projectJql + "&status=" + openStatus + "&maxResults=" + maxResults;
 
-console.log("Getting open issues using: " + searchService + openIssuesQueryString);
-
-var xmlHttpRequest = new XMLHttpRequest();
-var url = searchService + openIssuesQueryString;
-xmlHttpRequest.onreadystatechange = function () {
+function getEmptyReportersObject(parsedResponse) {
     "use strict";
-    if (xmlHttpRequest.readyState === 4 && xmlHttpRequest.status === 200) {
-        var parsedResponse = JSON.parse(xmlHttpRequest.responseText);
+    var reputationScore = {};
+    var reporter;
+    parsedResponse.issues.forEach(function (issue) {
+        reporter = issue.fields.reporter.name;
+        reputationScore[reporter] = 0;
+    });
+
+    return reputationScore;
+}
+
+function getReportersReputation(reporterName) {
+    "use strict";
+
+    var priorityChangesJql = "reporter=" + reporterName.replace(/@/g, "\\u0040") + "+and+priority+changed+and+status+was+" + resolvedStatus;
+    var changedPrioritiesUrl = searchService + "jql=" + priorityChangesJql + "&maxResults=" + maxResults;
+
+    var changedPrioritiesXhr = new XMLHttpRequest();
+    changedPrioritiesXhr.onreadystatechange = function () {
+
+        if (changedPrioritiesXhr.readyState === 4 && changedPrioritiesXhr.status === 200) {
+            var potentialInflatedIssues = JSON.parse(changedPrioritiesXhr.responseText);
+
+            if (potentialInflatedIssues.issues.length > 0) {
+                console.log("reporterName", reporterName, "potentialInflatedIssues", potentialInflatedIssues);
+            }
+        }
+    };
+
+    changedPrioritiesXhr.open("GET", changedPrioritiesUrl, true);
+    changedPrioritiesXhr.send();
+}
+
+var openIssuesXhr = new XMLHttpRequest();
+var openIssuesUrl = searchService + openIssuesQueryString;
+openIssuesXhr.onreadystatechange = function () {
+    "use strict";
+    if (openIssuesXhr.readyState === 4 && openIssuesXhr.status === 200) {
+        var parsedResponse = JSON.parse(openIssuesXhr.responseText);
         console.log("parsedResponse", parsedResponse);
 
-        var reporters = {};
-        var reporter;
+        var reputationScore = getEmptyReportersObject(parsedResponse);
 
-        parsedResponse.issues.forEach(function (issue) {
-            console.log("issue", issue);
-
-            reporter = issue.fields.reporter.name;
-            reporters[reporter] = true;
-        });
-
-        console.log("reporters", reporters);
+        console.log("reputationScore", reputationScore);
+        Object.keys(reputationScore).forEach(getReportersReputation);
     }
 
 };
 
-xmlHttpRequest.open("GET", url, true);
-xmlHttpRequest.send();
+openIssuesXhr.open("GET", openIssuesUrl, true);
+
+console.log("Getting open issues using: " + searchService + openIssuesQueryString);
+openIssuesXhr.send();
